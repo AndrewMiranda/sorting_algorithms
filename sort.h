ad33c1b9e93173026be969410df938d51742f936
AndrewMiranda <40378450+AndrewMiranda@users.noreply.github.com>
2021-03-12 20:18:45 -0500

    Update

--- sort.h
+++ sort.h
@@ -19,16 +19,25 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
+listint_t *create_listint(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-void counting_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+int lomuto_partision(int *array, size_t size, int first_elmt, int last_elmt);
+void method_quick_sort(int *array, size_t size, int left, int right);
+void swap(int *array, size_t size, int a, int b);
+int max_element(int *array, size_t size);
+int *max_length(int *array, unsigned int size);
 
 #endif
